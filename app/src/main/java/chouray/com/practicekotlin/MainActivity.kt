5ee8565befcf305ffe705890e3b15ff5ce382478
周雷 <lei.zhoulei@deepoon.com>
2018-03-07 14:40:19 +0800

    添加代码用于测试， Jenkins是否修改版本号成功

--- app/src/main/java/chouray/com/practicekotlin/MainActivity.kt
+++ app/src/main/java/chouray/com/practicekotlin/MainActivity.kt
@@ -2,11 +2,18 @@ package chouray.com.practicekotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, getVersionName(), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getVersionName() : String {
+        return packageManager.getPackageInfo(packageName, 0).versionName;
     }
 }
