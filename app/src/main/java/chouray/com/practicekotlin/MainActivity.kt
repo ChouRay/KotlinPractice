3ca76746c37028252bdf9fd04c2ba079f7841bc4
周雷 <lei.zhoulei@deepoon.com>
2018-03-07 17:57:30 +0800

    修改配置

--- app/src/main/java/chouray/com/practicekotlin/MainActivity.kt
+++ app/src/main/java/chouray/com/practicekotlin/MainActivity.kt
@@ -2,7 +2,7 @@ package chouray.com.practicekotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, getVersionName(), Toast.LENGTH_SHORT).show()
+        var tvVersionName = findViewById<TextView>(R.id.id_tv_versionname)
+        tvVersionName.setText("VersionName: " +getVersionName() + "\nVersionCode: " + getVersionCode())
+
     }
 
     private fun getVersionName() : String {
         return packageManager.getPackageInfo(packageName, 0).versionName;
+    }
+    private fun getVersionCode() : Int {
+        return packageManager.getPackageInfo(packageName, 0).versionCode;
     }
 }
